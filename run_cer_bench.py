ff76c46b2528d0a263ef5c760fc420eb16951f47
goncamateus <mgm4@cin.ufpe.br>
2021-08-02 19:27:27 -0300

    Adiciona por mes tambem

--- run_cer_bench.py
+++ run_cer_bench.py
@@ -21,13 +21,13 @@ FORWARD_EXPANSION = 1
 N_LAYERS = 1
 DROPOUT = 0.0
 DECOMP_METHOD = 'fft'
-MES = 'Dec'
 DECOMP = True
 DEVICE = torch.device('cuda')
 
 YELLOW = '\033[93m'
 RED = '\033[91m'
 GREEN = '\033[92m'
+BLUE = '\033[94m'
 ENDC = '\033[0m'
 
 PARQUES_DICT = {
@@ -42,6 +42,7 @@ PARQUES_DICT = {
     'Taiba': ['Aguia', 'Andorinha', 'Colonia']
 }
 
+MESES = ['Sep', 'Oct', 'Nov', 'Dec']
 TIME_STEPS = ['30_min', '1_day']
 
 
@@ -57,82 +58,84 @@ def main():
         print(f'{RED}Rodando Complexo de {complexo}:{ENDC}')
         for time_step in TIME_STEPS:
             print(f'{YELLOW}Rodando Time step {time_step}:{ENDC}')
-            for central in centrais:
-                print(f'{GREEN}Rodando Central {central}:{ENDC}')
-                if not os.path.exists(f'data/out/{central}_{MES}_{time_step}_SAETS'):
-                    os.system(
-                        f'mkdir data/out/{central}_{MES}_{time_step}_SAETS')
-                if not os.path.exists(f'data/out/{central}_{MES}_{time_step}_Cabral'):
-                    os.system(
-                        f'mkdir data/out/{central}_{MES}_{time_step}_Cabral')
-                dir_name = f'data/out/{central}_{MES}_{time_step}'
-                dataset = CERDataset(window=WINDOW,
-                                     horizons=HORIZONS,
-                                     complexo_eolico=complexo,
-                                     central_eolica=central,
-                                     time_step=time_step,
-                                     mes=MES,
-                                     decomp=DECOMP,
-                                     decomp_method=DECOMP_METHOD)
+            for mes in MESES:
+                print(f'{BLUE}Rodando Mes {mes}:{ENDC}')
+                for central in centrais:
+                    print(f'{GREEN}Rodando Central {central}:{ENDC}')
+                    if not os.path.exists(f'data/out/{central}_{mes}_{time_step}_SAETS'):
+                        os.system(
+                            f'mkdir data/out/{central}_{mes}_{time_step}_SAETS')
+                    if not os.path.exists(f'data/out/{central}_{mes}_{time_step}_Cabral'):
+                        os.system(
+                            f'mkdir data/out/{central}_{mes}_{time_step}_Cabral')
+                    dir_name = f'data/out/{central}_{mes}_{time_step}'
+                    dataset = CERDataset(window=WINDOW,
+                                         horizons=HORIZONS,
+                                         complexo_eolico=complexo,
+                                         central_eolica=central,
+                                         time_step=time_step,
+                                         mes=mes,
+                                         decomp=DECOMP,
+                                         decomp_method=DECOMP_METHOD)
 
-                train_loader = DataLoader(dataset, batch_size=128,
-                                          shuffle=True, num_workers=8)
-                input_example = next(iter(train_loader))[0]
-                input_size = input_example.shape[1]*input_example.shape[2]
-                auto_encoder = AutoEncoder(input_size=input_size,
-                                           horizons=HORIZONS, device=DEVICE,
-                                           forward_expansion=FORWARD_EXPANSION,
-                                           num_layers=N_LAYERS,
-                                           dropout=DROPOUT)
+                    train_loader = DataLoader(dataset, batch_size=128,
+                                              shuffle=True, num_workers=8)
+                    input_example = next(iter(train_loader))[0]
+                    input_size = input_example.shape[1]*input_example.shape[2]
+                    auto_encoder = AutoEncoder(input_size=input_size,
+                                               horizons=HORIZONS, device=DEVICE,
+                                               forward_expansion=FORWARD_EXPANSION,
+                                               num_layers=N_LAYERS,
+                                               dropout=DROPOUT)
 
-                trainer = Trainer(gpus=1, max_epochs=5)
-                trainer.fit(auto_encoder, train_dataloader=train_loader)
-                final_model = FinalModule(input_size=input_size,
-                                          horizons=HORIZONS, device=DEVICE,
-                                          forward_expansion=FORWARD_EXPANSION,
-                                          num_layers=N_LAYERS,
-                                          dropout=DROPOUT)
-                final_model.load_encoder(auto_encoder.encoder)
-                trainer = Trainer(gpus=1, max_epochs=5)
-                trainer.fit(final_model, train_dataloader=train_loader)
+                    trainer = Trainer(gpus=1, max_epochs=5)
+                    trainer.fit(auto_encoder, train_dataloader=train_loader)
+                    final_model = FinalModule(input_size=input_size,
+                                              horizons=HORIZONS, device=DEVICE,
+                                              forward_expansion=FORWARD_EXPANSION,
+                                              num_layers=N_LAYERS,
+                                              dropout=DROPOUT)
+                    final_model.load_encoder(auto_encoder.encoder)
+                    trainer = Trainer(gpus=1, max_epochs=5)
+                    trainer.fit(final_model, train_dataloader=train_loader)
 
-                dataset_lucas = CERDataset(window=WINDOW,
-                                           horizons=HORIZONS,
-                                           complexo_eolico=complexo,
-                                           central_eolica=central,
-                                           time_step=time_step,
-                                           mes=MES,
-                                           decomp=DECOMP,
-                                           decomp_method='2fft')
+                    dataset_lucas = CERDataset(window=WINDOW,
+                                               horizons=HORIZONS,
+                                               complexo_eolico=complexo,
+                                               central_eolica=central,
+                                               time_step=time_step,
+                                               mes=mes,
+                                               decomp=DECOMP,
+                                               decomp_method='2fft')
 
-                train_loader_lucas = DataLoader(dataset_lucas, batch_size=128,
-                                                shuffle=True, num_workers=8)
-                input_example_lucas = next(iter(train_loader_lucas))[0]
-                mlp = MLPLucas(window_size=input_example_lucas.shape[1],
-                               n_comps=input_example_lucas.shape[2],
-                               horizons=HORIZONS)
-                trainer = Trainer(gpus=1, max_epochs=5)
-                trainer.fit(mlp, train_dataloader=train_loader_lucas)
+                    train_loader_lucas = DataLoader(dataset_lucas, batch_size=128,
+                                                    shuffle=True, num_workers=8)
+                    input_example_lucas = next(iter(train_loader_lucas))[0]
+                    mlp = MLPLucas(window_size=input_example_lucas.shape[1],
+                                   n_comps=input_example_lucas.shape[2],
+                                   horizons=HORIZONS)
+                    trainer = Trainer(gpus=1, max_epochs=5)
+                    trainer.fit(mlp, train_dataloader=train_loader_lucas)
 
-                dataset.set_type('test')
-                dataset_lucas.set_type('test')
-                mlp = mlp.cpu()
-                final_model = final_model.cpu()
-                X_test = dataset.samples
-                X_test_lucas = dataset_lucas.samples
-                y = dataset.labels
-                y_final = final_model(X_test).detach() / \
-                    dataset.test_scaler.scale_
-                y_mlp = mlp(X_test_lucas).detach() / \
-                    dataset_lucas.test_scaler.scale_
+                    dataset.set_type('test')
+                    dataset_lucas.set_type('test')
+                    mlp = mlp.cpu()
+                    final_model = final_model.cpu()
+                    X_test = dataset.samples
+                    X_test_lucas = dataset_lucas.samples
+                    y = dataset.labels
+                    y_final = final_model(X_test).detach() / \
+                        dataset.test_scaler.scale_
+                    y_mlp = mlp(X_test_lucas).detach() / \
+                        dataset_lucas.test_scaler.scale_
 
-                y_final = y_final.numpy()
-                y_mlp = y_mlp.numpy()
-                for i in range(y.shape[1]):
-                    save_pred(y_final[:, i], horizon=i,
-                              dir_name=dir_name + '_SAETS')
-                    save_pred(y_mlp[:, i], horizon=i,
-                              dir_name=dir_name + '_Cabral')
+                    y_final = y_final.numpy()
+                    y_mlp = y_mlp.numpy()
+                    for i in range(y.shape[1]):
+                        save_pred(y_final[:, i], horizon=i,
+                                  dir_name=dir_name + '_SAETS')
+                        save_pred(y_mlp[:, i], horizon=i,
+                                  dir_name=dir_name + '_Cabral')
 
 
 if __name__ == "__main__":
